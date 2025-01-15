4d7c512f1e2b9d6e1c4d605b2a598aab29e840cd
gwpark <rmsdn0415@naver.com>
2025-01-15 18:49:23 +0900

    feat: CORS CONFIG 추가

--- backend/src/main/java/com/blockchain/backend/config/WebConfig.java
+++ backend/src/main/java/com/blockchain/backend/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    //SpringBoot <-----> React CORS CONFIG 추가
+    //Spring Security를 사용하는 경우 SecurityzConfig에서 추가설정이 필요함.
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
